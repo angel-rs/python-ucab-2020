2f8c2fe1ce69f3388ea90d24ee7778b7fb3a8a9a
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-11-14 19:11:56 -0400

    Add assignment2 placeholder

---
+++ assignments/assignment2.py
@@ -0,0 +1,113 @@
+# 31/10/2020
+# Angel Rodríguez
+# CI: 27015036
+# Realizado usando python 3.7.7
+
+# 1. Escriba un programa que reciba como entrada un número entero n mayor a 0, y la salida
+# sea de la forma 123…n, donde … son los números que faltan en la secuencia. Ejemplo:
+# Si la entrada n = 5, la salida es 12345
+
+def ejercicio1(n = 1):
+    # TODO
+
+# 2. Escriba una función llamada rotate() que reciba como argumento una lista y un entero n y
+# rote los elemento de la lista de acuerdo a ese número n. Ejemplo, lista de entrada: [1, 2, 3,
+# 4, 5, 6 ] ; n= 2. La salida debe ser [3, 4, 5, 6, 1, 2]
+
+def ejercicio2():
+    # TODO
+
+# 3. Escriba una función llamada delete_duplicates(), que reciba una lista con varios elemento
+# duplicados y la salida sea la lista sin duplicados. Ejm: lista de entrada [12, 24, 12, 56, 3, 14,
+# 56, 3, 12], la salida debe ser [12, 24, 56, 3, 14]
+
+
+def ejercicio3():
+    # TODO
+
+# 4. Escriba un programa que dado tres tuples con nombres, edades y ciudades. Como entrada
+# solicite un numero n y en la salida indique el nombre, edad y ciudad correspondiente a es
+# número. Ejemplo:
+# nombres = (“Juan”, “Maria”, “Ricardo”, “Ana”, “Cecilia”)
+# edades = (23, 21, 19, 25, 22)
+# ciudades = (“Maracay”, “Valencia”, “Caracas”, “Barquisimeto”, “Barcelona”)
+# n = 2, la salida será: Ricardo 19 Caracas
+
+def ejercicio4():
+    # TODO
+
+# 5. Escriba un programa que dado una lista de tuples que representan un nombre y una edad,
+# encontrar la persona de mayor edad. Ejm: [(“Antonio”, 21), (“Roberto”, 18), (“Teresa”, 19)],
+# la salida debe ser: Antonio 21
+
+def ejercicio5():
+    # TODO
+
+# 6. Escriba una función llamada find_indexes() que reciba como parámetro un string y un
+# substring, y la salida sea un tuple que indique los índices de ocurrencia de ese substring en
+# el primer string. Ejm, string = “tattattata” y el substring es “tt”, la salida debe ser (2, 5)
+
+def ejercicio6():
+    # TODO
+
+# 7. Escriba una función llamada sumNumbers(), que reciba como entrada un string que
+# contiene caracteres numéricos y la salida muestre la suma de tales valores. Ejm: entrada:
+# abc1234xyz, la salida debe ser 1234; entrada = aa11b33, salida 44; entrada: pueblo, salida 0.
+
+def ejercicio7():
+    # TODO
+
+# 8. Escriba una función llamada primeOrComposite(), que reciba como argumento un número
+# n positivo y la salida sea Primo si es primo o Compuesto si no lo es, Ejm: entrada 47, salida
+# Primo, entrada 16, salida Compuesto
+
+def ejercicio8():
+    # TODO
+
+# 9. Escriba una función que reciba como entrada una lista y la salida sea la lista con los
+# elementos intercambiados, ejemplo: entrada: [1, 2, 3, 4, 5], salida: [2, 1, 4, 3, 5]
+
+def ejercicio9():
+    # TODO
+
+# 10. La función zip() de Python, toma dos o más objetos iterables y retorna una lista de tuples
+# con un elemento de cada lista de entrada, de acuerdo a su índice. Ejemplo entrada, paises
+# = ["China", "India", "Estados Unidos", "Indonesia"] ; poblaciones = [1391, 1364, 327, 264]. 
+# La salida es [('China', 1391), ('India', 1364), ('Estados Unidos', 327), ('Indonesia', 264)].
+# Escriba una función llamada my_zip(), que realice lo mismo (limítelo a sólo dos iterables).
+
+def ejercicio10():
+    # TODO
+
+# 11. Escriba una función lambda que encuentre el valor de 𝑒^𝑥
+# dada por la expresión ((1 + 1/n)^n)^x
+# para un valor de n= 1000.
+
+def ejercicio11():
+    # TODO
+
+## 12. Escriba una función lambda que obtenga el primer y último valor de una lista.
+
+def ejercicio12():
+    # TODO
+
+# 13. Escriba un generador usando una compresión de lista para indicar en una lista de enteros
+# cuales son pares y cuales impares, por ejemplo, entrada: [1, 2, 3 , 4] salida: [“impar”, “par”,
+# “impar”, “par”]. Ayuda: recuerde la forma abreviada del if
+
+def ejercicio13():
+    # TODO
+
+# 14. Escriba una función map, para que dada una lista con nombres, la salida sea la lista con los
+# nombres en Mayúsculas. Ejm: entrada [“alfredo”, “luis”, “margarita”, “rosa”], salida
+# [“ALFREDO”, “LUIS”, “MARGARITA”, “ROSA”] 
+
+def ejercicio14():
+    # TODO
+
+# 15. Escriba una función filter, que aplicada a una lista con notas de alumnos, devuelva a la salida
+# una lista con aquellas notas superiores a 17. Ejm: entrada [13, 20, 15, 18, 12, 19, 14, 17,18]
+# la salida debe ser [20, 18, 19, 18]
+
+def ejercicio14():
+    # TODO
